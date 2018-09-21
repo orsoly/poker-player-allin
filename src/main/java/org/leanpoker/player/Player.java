2ebeb7421aa87d1726a759487e68d1fe8d38ff0f
Kiss Orsolya <orsolya.kiss@ericsson.com>
2018-09-21 13:35:18 +0200

    Parse json element

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,29 @@ public class Player {
     static final String VERSION = "Original Roulette Banking Victorious Clan";
 
     public static int betRequest(JsonElement request) {
+        Hand hand = new Hand();
+        getCard(request, hand);
         return 10000;
+    }
+
+    public static void getCard(JsonElement request, Hand hand) {
+        for(int i=0; i<2; i++) {
+            String rank = request.getAsJsonArray("players").get(1).getAsJsonArray("hole_cards").get(i).get("rank").getAsString();
+            String suit = reguest.getAsJsonArray("players").get(1).getAsJsonArray("hole_cards").get(i).get("suit").getAsString();
+            Card card = new Card(rank, suit);
+            putIntoHand(hand, card);
+        }
+        for (int i=0; i<reguest.getAsJsonArray("community_cards").lenght(); i++) {
+            reguest.getAsJsonArray("community_cards").get(i).get("rank").getAsString();
+            reguest.getAsJsonArray("community_cards").get(i).get("suit").getAsString();
+            Card card = new Card(rank, suit);
+            putIntoHand(hand, card);
+        }
+    }
+
+
+    public static void putIntoHand(Hand h, Card c) {
+        h.add(c);
     }
 
     public static void showdown(JsonElement game) {
